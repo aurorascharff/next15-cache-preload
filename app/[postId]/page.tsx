d63d2c568945162380930a0fccb14557e57ad86a
Aurora Scharff <aurora.sofie@gmail.com>
2025-02-25 00:10:40 +0100

    Update

---
+++ app/[postId]/page.tsx
@@ -0,0 +1,85 @@
+import { cache, Suspense } from 'react';
+
+const getPost = async (postId: string) => {
+  await new Promise(resolve => {
+    return setTimeout(resolve, 1000);
+  });
+  return [
+    {
+      body: 'This is the first post on this blog.',
+      id: 1,
+      title: 'Hello World',
+    },
+    {
+      body: 'This is the second post on this blog.',
+      id: 2,
+      title: 'Hello Next.js',
+    },
+  ].find(post => {
+    return post.id.toString() === postId;
+  });
+};
+
+const getComments = cache(async (postId: string) => {
+  await new Promise(resolve => {
+    return setTimeout(resolve, 1000);
+  });
+  return [
+    {
+      body: 'This is the first comment on this blog.',
+      id: 1,
+      postId: 1,
+    },
+    {
+      body: 'This is the second comment on this blog.',
+      id: 2,
+      postId: 1,
+    },
+  ].filter(comment => {
+    return comment.postId.toString() === postId;
+  });
+});
+
+async function Comments({ postId }: { postId: string }) {
+  const comments = await getComments(postId);
+
+  return (
+    <>
+      <h2>Comments</h2>
+      <ul>
+        {comments.map(comment => {
+          return <li key={comment.id}>{comment.body}</li>;
+        })}
+      </ul>
+    </>
+  );
+}
+
+async function Post({ postId }: { postId: string }) {
+  const post = await getPost(postId);
+
+  return (
+    <>
+      <h2>Title: {post?.title}</h2>
+      Post comments:
+      <Suspense fallback={<div>Loading comments...</div>}>
+        <Comments postId={postId} />
+      </Suspense>
+    </>
+  );
+}
+
+export default async function PostPage({ params }: { params: Promise<{ postId: string }> }) {
+  const { postId } = await params;
+
+  getComments(postId);
+
+  return (
+    <div>
+      <h1>Post: {postId}</h1>
+      <Suspense fallback={<div>Loading post...</div>}>
+        <Post postId={postId} />
+      </Suspense>
+    </div>
+  );
+}
